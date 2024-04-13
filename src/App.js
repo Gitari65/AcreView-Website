787378e07be98989635e886488830382fcd789eb
Gitari65 <agitari65@gmail.com>
2024-04-13 15:27:04 +0300

    changes in navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import TopBar from './components/TopBar';
+import NavBar from './components/NavBar';
+import CouraselComponent from './components/Courasel';
+import NavigationBar from './components/NavigationBar';
+import { useEffect, useState } from 'react';
+
+// import '../src/assets/styles/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import 'bootstrap/dist/js/bootstrap.js'
+
+
 
 function App() {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+      const handleScroll = () => {
+          if (window.scrollY > 100) {
+              setIsSticky(true);
+          } else {
+              setIsSticky(false);
+          }
+      };
+
+      window.addEventListener('scroll', handleScroll);
+
+      return () => {
+          window.removeEventListener('scroll', handleScroll);
+      };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      {/*
+      <NavBar/>
+      <Courasel/> */}
+      {isSticky ?App(
+        <>
+          <NavigationBar/>
+      <CouraselComponent/> 
+        </>
+      ):(  <>
+        <TopBar /> 
+      <NavigationBar/>
+      <CouraselComponent/>
+      </>  )}
+   
+      
     </div>
   );
 }
