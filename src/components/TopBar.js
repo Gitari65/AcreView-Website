cb2244f04d9ecc558d796336e027ceabf800adf2
Gitari65 <agitari65@gmail.com>
2024-04-27 20:53:47 +0300

    changes on routes with react router dom

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,11 +2,14 @@ import React from 'react'
 //import facebook,titter and linked from fontawesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebookF, faTwitter, faLinkedinIn, faInstagram } from '@fortawesome/free-brands-svg-icons'
-
+import{faPhoneAlt,faEnvelopeOpen,faClock} from '@fortawesome/free-solid-svg-icons'
 const facebookIcon = <FontAwesomeIcon icon={faFacebookF} />
 const twitterIcon = <FontAwesomeIcon icon={faTwitter} />
 const linkedinIcon = <FontAwesomeIcon icon={faLinkedinIn} />
 const instagramIcon = <FontAwesomeIcon icon={faInstagram} />
+const phoneIcon = <FontAwesomeIcon icon={faPhoneAlt} />
+const emailIcon = <FontAwesomeIcon icon={faEnvelopeOpen} />
+const clockIcon = <FontAwesomeIcon icon={faClock} />
 
 const TopBar = () => {
   return (
@@ -14,20 +17,25 @@ const TopBar = () => {
             {/* <!-- Topbar Start --> */}
             
     <div className="container-fluid  top-bar text-white-50 py-2 px-0 d-none d-lg-block top-bar">
-      <div className="row gx-0 align-items-center">
+      <div className="row gx-0 align-items-center  top-bar-text ">
         <div className="col-lg-7 px-5 text-start">
           <div className="h-100 d-inline-flex align-items-center me-4">
-            <small className="fa fa-phone-alt me-2"></small>
-            <small>+254729-569800</small>
+           <p >{phoneIcon}
+            <small> +254729-569800</small></p> 
           </div>
           <div className="h-100 d-inline-flex align-items-center me-4">
-            <small className="far fa-envelope-open me-2"></small>
-            <small>geoacresurveys@yahoo.com</small>
+          <p>
+          {emailIcon}
+            <small> geoacresurveys@yahoo.com</small>
+          </p>
           </div>
           <div className="h-100 d-inline-flex align-items-center me-4">
-            <small className="far fa-clock me-2"></small>
-            <small>Mon - Fri : 09 AM - 09 PM</small>
+          <p>
+          {clockIcon}
+            <small> Mon - Fri : 09 AM - 05 PM</small>
+             </p>
           </div>
+         
         </div>
         <div className="col-lg-5 px-5 text-end">
           <div className="h-100 d-inline-flex align-items-center">
