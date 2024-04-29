29c81c1a8bbc4b60f5f2802e2ff5388afa1c890c
Gitari65 <agitari65@gmail.com>
2024-04-29 12:12:33 +0300

    adding react-wow animatin

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,7 +11,7 @@ function Contact() {
           </h1>
         </div>
         <div className="row">
-          <div className="contact-form col-lg-6 col-md-5 col-sm-12 mb-5">
+          <div className="contact-form col-lg-6 col-md-5 col-sm-12 mb-5 wow fadeInLeft">
             <form>
               <h1>Send us a Message.</h1>
               <div className="form-group">
@@ -29,7 +29,7 @@ function Contact() {
               <button type="submit" className="btn btn-primary">Submit</button>
             </form>
           </div>
-          <div className="iframe-container col-lg-5 col-md-6 col-sm-12">
+          <div className="iframe-container col-lg-5 col-md-6 col-sm-12 wow fadeInRight">
             <iframe
               title="Edipark Plaza, Thika"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.965098441685!2d37.0144013!3d-0.993887!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f4d073e1de2b3%3A0xae33f51618a7fe25!2sEdipark%20Plaza%2C%20Thika%20Greens%2C%20Kahaini!5e0!3m2!1sen!2ske!4v1649850219735!5m2!1sen!2ske"
